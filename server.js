0451ca6ecb39fa67799ac699859740890ae8380a
Putti <putti.tang@gmail.com>
2020-09-08 23:30:38 +0700

    last commit of today

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(port);
 console.log('todo list RESTful API server startedd on: ' + port);
 
 app.get("/", (req, res, next) => {
-  console.log("/ -- node03");
+  console.log("/ -- hello world: main-app");
   res.status(200).send('hello world from main-app');
 });
 
@@ -20,7 +20,7 @@ app.get("/key1", (req, res, next) => {
 
 app.get("/health", (req, res, next) => {
   console.log("log /health");
-  res.status(200).send('log /health of node03');
+  res.status(200).send('log /health of main-app');
 });
 
 REDIS_PORT = process.env.redis_port;
@@ -52,27 +52,7 @@ app.get("/test-redis", (req, res, next) => {
     
 });
 
-app.get("/node03/color", (req, res, next) => {
-  res.status(200).send('OK.BlueBluePattaya');
-});
 
-app.get("/node03/service2", (req, res, next) => {
-  res.status(200).send('OK.OK.Service2');
-});
-
-app.get("/node03/service3", (req, res, next) => {
-  res.status(200).send('OK.OK.Service3');
-});
-
-app.get("/node03/loginService", (req, res, next) => {
-
-  console.log("DEBUG (1)");
-  console.log("INFO (2)(3)");
-  const URL_SERVICE1 = process.env.URL_SERVICE1;
-  const URL_SERVICE2 = process.env.URL_SERVICE2;
-  res.json([URL_SERVICE1, "Lisa-xxxyy", "Michael", URL_SERVICE2, req.query.userId]);
-  
-});
 
 app.use(express.json());
 
