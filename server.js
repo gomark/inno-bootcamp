308b9ed62a78990f14d4bf8d8af61fcd295e2b86
Putti <putti.tang@gmail.com>
2020-09-08 22:44:43 +0700

    test redis

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/key1", (req, res, next) => {
-  console.log("/node03");
+  console.log("/key1");
   res.status(200).send('key1=' + process.env.key1);
 });
 
@@ -23,9 +23,22 @@ app.get("/health", (req, res, next) => {
   res.status(200).send('log /health of node03');
 });
 
-app.get("/node03/health", (req, res, next) => {
-  console.log('log node03/health - rev15');
-  res.status(200).send('log node03/health of node03 - rev15');
+REDIS_PORT = process.env.redis_port;
+REDIS_HOST = process.env.redis_host;
+
+const redis = require("redis");
+const client = redis.createClient(REDIS_PORT, REDIS_HOST);
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+app.get("/test-redis", (req, res, next) => {
+  console.log('/test-redis');
+  client.set("key", "value", redis.print);
+  key = client.get("key", redis.print);
+  res.status(200).send('key=' + key);
+    
 });
 
 app.get("/node03/color", (req, res, next) => {
---
+++ set-env.sh
@@ -0,0 +1,2 @@
+export redis_host=34.126.92.90
+export redis_port=6379
